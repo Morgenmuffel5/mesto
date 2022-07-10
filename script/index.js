7ecb61a0d676bbdc7a52ee5630cae7a2bd7f26e1
Anna Lavrishcheva <annalavrishcheva5@yandex.ru>
2022-07-10 19:36:21 +0300

    fix:исправить файл JS в соответствии с комментариями ревьюера 1 иттерация

--- script/index.js
+++ script/index.js
@@ -1,54 +1,41 @@
+//объявление переменных
+let profileChangeButton = document.querySelector('.profile__change-button');
+let popup = document.querySelector('.popup'); // секция popup
+let popupInputName = document.querySelector('.popup__input-name');//поле ввода имени в форме
+let profileName = document.querySelector('.profile__name'); // поле имени в секции profile
+let popupInputAbout = document.querySelector('.popup__input-about'); //поле ввода инфо о себе в форме
+let profileAbout = document.querySelector('.profile__specialization');// поле инфо о себе в секции profile
+let popupSaveButton = document.querySelector('.popup__save-button');//кнопка сохранить
+let profileCloseButton = document.querySelector('.popup__close-button');//кнопка закрыть
+let popupForm = document.querySelector('.popup__form');//сама форма
+
+//ФУНКЦИИ
 //открытие popup и заполнение полей данными, указанными в профайле
-let profileChangeButton = document.querySelector('.profile_change-button');
-
 function popupOpen() {
-    let popup = document.querySelector('.popup');
-    let popupInputName = document.querySelector('.popup__input-name');//поле ввода имени в форме
-    let profileName = document.querySelector('.profile__name'); // поле имени в секции profile
-    let popupInputAbout = document.querySelector('.popup__input-about'); //поле ввода инфо о себе в форме
-    let profileAbout = document.querySelector('.profile__specialization');// поле инфо о себе в секции profile
-
     popupInputName.value = profileName.textContent; // присваиваем value полю формы, равное значению имени пользователя
     popupInputAbout.value = profileAbout.textContent;// присваиваем value полю формы, равное значению инфо о себе
-
-    return popup.classList.add('popup_opened');
+    popup.classList.add('popup_opened');
 }
-
-profileChangeButton.addEventListener('click', popupOpen);
-
-
-
 
 
 //Закрытие попапа
-
-let popupSaveButton = document.querySelector('.popup__save-button');//кнопка сохранить
-let profileCloseButton = document.querySelector('.popup__close-button');//кнопка закрыть
-
 function popupClose() { //функция простого закрытия
-    let popup = document.querySelector('.popup'); // секция popup
-    return popup.classList.remove('popup_opened');//удаление класса открытия попапа
+    popup.classList.remove('popup_opened');//удаление класса открытия попапа
 }
 
-function closePopupAndSaveData () { //функция сохранения и закрытия
-    let popupForm = document.querySelector('.popup__form');//сама форма
-    
-    function formSubmitHandler (evt) { //функция сохранения измененных значений value
-        evt.preventDefault(); 
-        let popupInputName = document.querySelector('.popup__input-name');//поле ввода имени в форме
-    let profileName = document.querySelector('.profile__name'); // поле имени в секции profile
-    let popupInputAbout = document.querySelector('.popup__input-about'); //поле ввода инфо о себе в форме
-    let profileAbout = document.querySelector('.profile__specialization');// поле инфо о себе в секции profile
-        profileName.textContent = popupInputName.value;
-        profileAbout.textContent = popupInputAbout.value;
-    }
-    
-    popupForm.addEventListener('submit', formSubmitHandler); //сохранить данные
+function formSubmitHandler(evt) { //функция сохранения измененных значений value
+    evt.preventDefault();
+    profileName.textContent = popupInputName.value;
+    profileAbout.textContent = popupInputAbout.value;
     popupClose();//закрыть форму
-    }
+}
+popupForm.addEventListener('submit', formSubmitHandler); //сохранить данные
 
-    //логика сохранения и закрытия
-popupSaveButton.addEventListener('click', closePopupAndSaveData);//при нажатии сохранить, реализуется функция сохранения данных и закрытия формы
+
+
+    //СОБЫТИЯ
+profileChangeButton.addEventListener('click', popupOpen); //открытие попапа
+popupForm.addEventListener('submit', formSubmitHandler); //сохранить данные
 profileCloseButton.addEventListener('click', popupClose)// просто закрыть форму
 
 
